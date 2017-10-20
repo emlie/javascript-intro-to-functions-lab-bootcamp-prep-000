8f5e8f0068c148ad18bca6077cdb5ed32f2322e1
Mia Elisenberg <mi-ya@live.no>
2017-10-20 14:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,12 +25,15 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === lowercase) {
-    return "I can't hear you!"
+    return "I can\'t hear you!"
   }
   else if (string === uppercase) {
     return 'YES INDEED!'
   }
   else if (string === 'I love you, Grandma') {
     return 'I love you, too'
+  }
+  else {
+    return 'What?'
   }
 }
